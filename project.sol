bc60926d9926a559139341021fd751e057a89a1c
Lav Kalsi <152765866+LavKalsi@users.noreply.github.com>
2024-06-24 20:21:53 +0530

    Add files via upload

---
+++ project.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract FunctionsAndErrors {
+   
+    string public name = "OrbitToken";
+    uint public supply = 0;
+
+    mapping (address => uint) public balances;
+
+    function mint(address _address, uint _value) public {
+        require(_address != address(0), "Invalid address");
+        require(_value > 0, "Mint value must be greater than zero");
+
+        supply += _value;
+        balances[_address] += _value;
+
+        assert(supply >= _value); 
+        assert(balances[_address] >= _value);
+    }
+
+    function burn(address _address, uint _value) public {
+      
+        if (balances[_address] < _value) {
+            revert("Insufficient balance to burn");
+        }
+
+        supply -= _value;
+        balances[_address] -= _value;
+
+        assert(supply >= 0);
+        assert(balances[_address] >= 0);
+    }
+}
+ 
